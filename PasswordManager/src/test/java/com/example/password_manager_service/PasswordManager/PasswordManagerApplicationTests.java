f48f9d3a549fec3cb2b84b10d20eaf6c03330bff
Абросимова Екатерина Андреевна <katya-telepneva@mail.ru>
2025-05-14 19:03:07 +0300

    Initial commit

---
+++ PasswordManager/src/test/java/com/example/password_manager_service/PasswordManager/PasswordManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.password_manager_service.PasswordManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
